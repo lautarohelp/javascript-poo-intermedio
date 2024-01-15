16327fd6cf8579cac18723891c909e744b2b6675
lautarohelp <lautarotoledo391@gmail.com>
2024-01-15 13:07:40 -0600

    Object.defineProperty

--- main.js
+++ main.js
@@ -13,11 +13,37 @@ const Lautaro = {
 console.log(Object.getOwnPropertyNames(Lautaro));
 console.log(Object.entries(Lautaro)); */
 
-console.log(Object.getOwnPropertyDescriptors(Lautaro));
+/* 
+Object.defineProperty(Lautaro, "navigator", {
+  value: "Chrome",
+  enumerable: false, no la podemos enumerar cuando hacemos keys.
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(Lautaro, "editor", {
+  value: "VS Code",
+  enumerable: true,
+  writable: false, no podemos editarla
+  configurable: true,
+});
+
+Object.defineProperty(Lautaro, "terminal", {
+  value: "WSL",
+  enumerable: true,
+  writable: true,
+  configurable: false, no podemos borrarla
+});
 
 Object.defineProperty(Lautaro, "pruebaNASA", {
   value: "extraterrestres",
-  enumerable: true,
-  writable: true,
-  configurable: true,
-})
+  enumerable: false,
+  writable: false,
+  configurable: false,
+}); 
+*/
+
+Object.seal(Lautaro); //hace el configurable se ponga el falls
+Object.freeze(Lautaro); //nos impide borrar y editar
+
+console.log(Object.getOwnPropertyDescriptors(Lautaro));
