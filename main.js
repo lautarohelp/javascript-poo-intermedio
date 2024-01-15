2a2f07857f56cbf429c53563d7ff99ed8c3e375e
lautarohelp <lautarotoledo391@gmail.com>
2024-01-15 12:18:25 -0600

    Métodos estáticos del prototipo Object

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello, POO en JS intermedio");
+const Lautaro = {
+  name: "Lautarito",
+  age: 18,
+  approvedCourses: ["Curso 1"],
+  addCourse(newCourse) {
+    console.log("This", this);
+    console.log("This.approvedCourses", this.approvedCourses);
+    this.approvedCourses.push(newCourse);
+  }
+}
+
+/* console.log(Object.keys(Lautaro));
+console.log(Object.getOwnPropertyNames(Lautaro));
+console.log(Object.entries(Lautaro)); */
+
+console.log(Object.getOwnPropertyDescriptors(Lautaro));
+
+Object.defineProperty(Lautaro, "pruebaNASA", {
+  value: "extraterrestres",
+  enumerable: true,
+  writable: true,
+  configurable: true,
+})
